ef84cf9d5e58b2733a542f9151e268da8d8651bd
thithomazelli <thiago.thomazelli@gmail.com>
2019-02-25 10:51:56 -0300

    Creating the initial structure to the TSI.GymTech.Repository project.

---
+++ TSI.GymTech.Repository/Repository.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TSI.GymTech.Data;
+using TSI.GymTech.Repository.Interfaces;
+
+namespace TSI.GymTech.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        //Fields
+        private readonly Context context;
+
+        //Constructor
+        public Repository()
+        {
+            context = new Context();
+        }
+
+        public T GetById(int id)
+        {
+            return context.Set<T>().Find(id);
+        }
+
+        public T GetByName(string name)
+        {
+            return context.Set<T>().Find(name);
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return context.Set<T>();
+        }
+
+        public IQueryable<T> query(System.Linq.Expressions.Expression<Func<T, bool>> filter)
+        {
+            return context.Set<T>().Where(filter);
+        }
+
+        public void Add(T entity)
+        {
+            context.Set<T>().Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            //Remember to reference System.Data.Entity
+            context.Entry<T>(entity).State = EntityState.Modified;
+        }
+
+        public void Remove(T entity)
+        {
+            context.Set<T>().Remove(entity);
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+    }
+}
