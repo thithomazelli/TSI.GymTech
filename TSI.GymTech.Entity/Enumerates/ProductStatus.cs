727f0c1cc9d2a21d1348c88d29ef0bbbde3f50c8
rogzamp <rogzampieri@gmail.com>
2019-04-01 23:07:35 +0100

    Changes enum according to the value from the database (0 Inactive / 1 Active)

--- TSI.GymTech.Entity/Enumerates/ProductStatus.cs
+++ TSI.GymTech.Entity/Enumerates/ProductStatus.cs
@@ -9,9 +9,9 @@ namespace TSI.GymTech.Entity.Enumerates
 {
     public enum ProductStatus
     {
-        [Display(ResourceType = typeof(App_LocalResources.ProductStatus), Name = "Active")]
-        Active = 0,
         [Display(ResourceType = typeof(App_LocalResources.ProductStatus), Name = "Inactive")]
-        Inactive = 1,
+        Inactive = 0,
+        [Display(ResourceType = typeof(App_LocalResources.ProductStatus), Name = "Active")]
+        Active = 1,
     }
 }
